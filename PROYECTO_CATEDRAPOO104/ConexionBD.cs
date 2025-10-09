9936bf0deae265ea66bda44387594bcf900ea84d
JL <japerezw25@gmail.com>
2025-10-08 21:48:51 -0600

    Se agrego Plantilla de cadena de conexión

--- PROYECTO_CATEDRAPOO104/ConexionBD.cs
+++ PROYECTO_CATEDRAPOO104/ConexionBD.cs
@@ -8,5 +8,37 @@ namespace PROYECTO_CATEDRAPOO104
 {
     internal class ConexionBD
     {
+
+        // Cadena de conexión a la base de datos
+        //CADENA DE CONEXION JL
+        //private readonly string cadenaConexion = "Data Source=HP-PAVILION-JL\\SQLDEVELOPER;Initial Catalog=ProyectoDeCatedra_FaseI;Integrated Security=True;Trust Server Certificate=True";
+        
+        //CADENA DE CONEXION 
+        private readonly string cadenaConexion = "Rellena con tu Data Source";
+
+        // Método para obtener una conexión abierta
+        public SqlConnection ObtenerConexion()
+        {
+            var conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+
+        // Método para testear la conexión
+        public bool ProbarConexion()
+        {
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    return conexion.State == System.Data.ConnectionState.Open;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
